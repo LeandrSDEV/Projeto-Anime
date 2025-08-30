1a65269fbae4dcd4d3f2a7e3d22ec1bc24683d4d
Leandro J. Santos <crazyg4ng@gmail.com>
2025-08-30 00:44:31 -0300

    Application: Criado a clase 'AnimeService' e a interface 'IAnimeservice' e finalizado os links da interface

---
+++ ProjetoAnime.Application/Interfaces/IAnimeService.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoAnime.Core.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAnime.Application.Interfaces
+{
+    public interface IAnimeService
+    {
+        Task<IEnumerable<Anime>> GetAllAnimesAsync();
+        Task<Anime> GetAnimeByIdAsync(int id);
+        Task<Anime> CreateAnimeAsync(Anime anime);
+        Task<Anime> UpdateAnimeAsync(Anime anime);
+        Task DeleteAnimeAsync(int id);
+    }
+}
