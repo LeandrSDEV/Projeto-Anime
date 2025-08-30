414c9fc08c53ba66fafe224e6c2bb17e3c2c95a2
Leandro J. Santos <crazyg4ng@gmail.com>
2025-08-30 00:04:12 -0300

    entidade Anime + propriedades básicas

--- ProjetoAnime.Core/Entidade/Anime.cs
+++ ProjetoAnime.Core/Entidade/Anime.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ProjetoAnime.Core.Entidade
 {
-    internal class Anime
+    public class Anime
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Diretor { get; set; }
+        public string Resumo { get; set; }
     }
 }
