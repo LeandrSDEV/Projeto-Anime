eaec43564be12448c8b442db24980341f9224d6c
Leandro J. Santos <crazyg4ng@gmail.com>
2025-08-31 11:52:04 -0300

    Serilog e exceções implantadas e rastreadas com geração de arquivo + console

---
+++ ProjetoAnime/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System.Net;
+using System.Text.Json;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Ocorreu uma exceção não tratada");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        // Definindo código de status apropriado conforme o tipo da exceção
+        context.Response.StatusCode = exception switch
+        {
+            KeyNotFoundException => (int)HttpStatusCode.NotFound,
+            ArgumentException => (int)HttpStatusCode.BadRequest,
+            _ => (int)HttpStatusCode.InternalServerError
+        };
+
+        var response = new
+        {
+            message = exception.Message,
+            statusCode = context.Response.StatusCode
+        };
+
+        var json = JsonSerializer.Serialize(response);
+
+        return context.Response.WriteAsync(json);
+    }
+}
--- ProjetoAnime/Program.cs
+++ ProjetoAnime/Program.cs
@@ -4,8 +4,25 @@ using ProjetoAnime.Application.Interfaces;
 using ProjetoAnime.Application.Queries;
 using ProjetoAnime.Infrastructure.Data;
 using ProjetoAnime.Infrastructure.Repositories;
+using Serilog;
+using Serilog.Events;
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File(
+    "Logs/error-.txt",
+    restrictedToMinimumLevel: LogEventLevel.Error,
+    rollingInterval: RollingInterval.Day,
+    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetAllAnimesQuery>());
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateAnimeCommand>());
@@ -28,6 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
