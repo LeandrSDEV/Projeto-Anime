350b45af5944b6b93c039bde732bd3746737db5a
Leandro J. Santos <crazyg4ng@gmail.com>
2025-08-31 23:44:04 -0300

    Adição do Docker-compose

--- ProjetoAnime/Program.cs
+++ ProjetoAnime/Program.cs
@@ -37,12 +37,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); 
+});
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
